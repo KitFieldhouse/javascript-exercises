36bc60da7e456dae643998af94037c8a510f8540
KitFieldhouse <kit.fieldhouse@gmail.com>
2023-04-02 19:58:25 -0500

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,36 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    if(typeof string != "string")
+    {
+        console.log("First argument MUST be of type string");
+        return "ERROR";
+    }
+    else if(typeof num != "number")
+    {
+        console.log("Second argument MUST be of type number");
+        return "ERROR";
+    }
+    else if(num < 0)
+    {
+        console.log("Second argument MUST be positive");
+        return "ERROR";
+    }
+
+    numberRound = Math.round(num);
+
+    if(numberRound != num)
+    {
+        console.log("Rounded non-int second argument...");
+    }
+
+    str = '';
+
+    for(let i = 0; i < num; i++)
+    {
+        str = str.concat(string);
+    }
+
+    return str;
 
 };
 
