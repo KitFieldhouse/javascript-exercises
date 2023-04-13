d267b7fcdfbb36154bda2c23431c70c2f8fb4b27
KitFieldhouse <kit.fieldhouse@gmail.com>
2023-04-13 14:19:06 -0500

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+let initialObj = {yearOfDeath: 0, yearOfBirth: 0};
+
+return arr.reduce(compareAge, initialObj);
 
 };
+
+function compareAge(currentOldest, el){
+
+    if(!("yearOfDeath" in el)){
+        el.yearOfDeath = (new Date()).getFullYear();
+    }
+
+    if ( (el.yearOfDeath - el.yearOfBirth ) > (currentOldest.yearOfDeath - currentOldest.yearOfBirth) )
+    {
+        return el;
+    }
+
+    return currentOldest
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
