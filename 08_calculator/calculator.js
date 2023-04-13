cbba184a1508f43806328b96b480f9463b0fea51
KitFieldhouse <kit.fieldhouse@gmail.com>
2023-04-13 13:19:53 -0500

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,60 @@
-const add = function() {
+const add = function(a,b) {
+  if(typeof a != 'number' || typeof b != 'number'){
+    return "ERR";
+  }
+
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+	  if(typeof a != 'number' || typeof b != 'number'){
+    return "ERR";
+  }
+  
+  return a - b;
+};
+
+const sum = function(arr) {
+
+  let isNumberArr = arr.filter((el) => !(typeof el == "number" ));
+
+  if(isNumberArr.length > 0){
+    return "ERR";
+  }
+
+  return arr.reduce((total, el) => el + total, 0 );
 	
 };
 
-const sum = function() {
+const multiply = function(arr) {
+
+  let isNumberArr = arr.filter((el) => !(typeof el == "number" ));
+
+  if(isNumberArr.length > 0){
+    return "ERR";
+  }
+
+  return arr.reduce((total, el) => el*total, 1);
+
+};
+
+const power = function(a,b) {
+
+  return Math.pow(a,b);
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
+const factorial = function(a) {
 	
-};
+  let prod = 1;
 
-const factorial = function() {
-	
+  for(let i = 1; i <= a ; i++){
+    prod = prod*i;
+  }
+
+  return prod;
+
 };
 
 // Do not edit below this line
