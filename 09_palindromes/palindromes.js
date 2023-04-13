531e44ecc019d507ec85311242b363fe39795655
KitFieldhouse <kit.fieldhouse@gmail.com>
2023-04-13 13:50:19 -0500

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (val) {
+
+    let processedStr = val.toLowerCase().split("").filter((el) => (el >= "a" && el <= "z")).join("");
+    let backwardsStr = processedStr.split("").reverse().join("");
+
+    return processedStr === backwardsStr;
 
 };
+
+//palindromes(" this is a test 1237898793v , !!! , ... of the function  "); // a test
 
 // Do not edit below this line
 module.exports = palindromes;
