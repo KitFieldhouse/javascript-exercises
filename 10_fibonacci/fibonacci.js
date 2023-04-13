f26ef46f67b37a982c1448ba91cb9c4347593cbc
KitFieldhouse <kit.fieldhouse@gmail.com>
2023-04-13 14:00:04 -0500

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = Number(index);
+    if (index < 0) {return "OOPS";}
+    else if(index < 3){return 1}
+
+    return fibonacci(index - 1) + fibonacci(index - 2);
 
 };
 
